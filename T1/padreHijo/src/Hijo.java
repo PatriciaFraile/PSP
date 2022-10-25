e332b5bc82663b96c5743591eed8ce33fc41ca1c
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-10-25 12:09:13 +0200

    subir

--- T1/padreHijo/src/Hijo.java
+++ T1/padreHijo/src/Hijo.java
@@ -1,13 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 
 public class Hijo {
-    Dni DNI = new Dni();
+
 
     public static void main(String[] args) {
-
         String leer;
+        String caracteres = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int[] numeros = new int[7];
+        int sumaNumeros = 0;
+        for (int i = 0; i < 7; i++) {
+           sumaNumeros = (int)(Math.random()*8+1);
+            numeros[i]=sumaNumeros;
+            numeros[i]++;
+            System.out.print(numeros[i]);
+        }
+        Object[][] dniCompleto = new Object[numeros][caracteres];
+
 
         try {
 
@@ -16,11 +27,13 @@ public class Hijo {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             leer = new String();
 
-            leer = br.readLine() + " tu DNI es: ";
+            leer = br.readLine() + " tu DNI es: "  ;
             System.out.println(leer);
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
+
+
 }
