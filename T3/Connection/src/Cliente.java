00a0ac562b073bd944a57bd13fbf4d922fa23b11
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-12-13 11:36:27 +0100

    subir

--- T3/Connection/src/Cliente.java
+++ T3/Connection/src/Cliente.java
@@ -1,3 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+
 public class Cliente {
+    public static void main(String[] args) {
+        PrintStream p;
+       // BufferedReader b;
+        try {
+            Socket cliente = new Socket("127.0.0.1", 1234);
+           p = new PrintStream(cliente.getOutputStream());
+           p.println("hola");
+          //  b = new BufferedReader( cliente.getInputStream());
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
 }
--- T3/Servidor/src/Servidor.java
+++ T3/Servidor/src/Servidor.java
@@ -1,2 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+    public static void main(String[] args) {
+        BufferedReader b;
+            try {
+                ServerSocket servidor = new ServerSocket(1234);
+                Socket cliente  = servidor.accept();
+                b = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                System.out.println(b.readLine());
+                //System.out.println( cliente.getPort());
+                cliente.close();//siempre hay que cerrar las conexiones
+                servidor.close();//siempre hay que cerrar las conexiones
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+    }
+
 }
