cf38b22068d0df997b51a34f9a01ecbd477f760d
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-12-21 11:06:47 +0100

    subir

--- T3/PracticaRed/src/Main.java
+++ T3/PracticaRed/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("La práctica este en los proyectos CLIENTE y SERVIDOR de este proyecto (PracticaRed)");
     }
 }
