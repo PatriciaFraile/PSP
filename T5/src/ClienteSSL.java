3fa92aaf5b4281b42487ed862938e36fddab6cbe
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2023-02-16 14:30:48 +0100

    subir

---
+++ T5/src/ClienteSSL.java
@@ -0,0 +1,38 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ClienteSSL {
+    public static void main(String[] args) throws Exception {
+        String Host = "localhost";
+        int puerto = 5556;//puerto remoto
+
+        // Propiedades JSSE)
+        System.setProperty("javax.net.ssl.trustStore","src/AlmacenSrv2");
+        System.setProperty("javax.net.ssl.trustStorePassword","1234567");
+
+        System.out.println("PROGRAMA CLIENTE INICIADO....");
+
+        SSLSocketFactory sfact = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket Cliente  = (SSLSocket) sfact.createSocket(Host, puerto);
+
+        // CREO FLUJO DE SALIDA AL SERVIDOR
+        DataOutputStream flujoSalida = new DataOutputStream(Cliente.getOutputStream());
+
+        // ENVIO UN SALUDO AL SERVIDOR
+        flujoSalida.writeUTF("Saludos al SERVIDOR DESDE EL CLIENTE");
+
+        // CREO FLUJO DE ENTRADA AL SERVIDOR
+        DataInputStream flujoEntrada = new DataInputStream(Cliente.getInputStream());
+
+        // EL servidor ME ENVIA UN MENSAJE
+        System.out.println("Recibiendo del SERVIDOR: \n\t" + flujoEntrada.readUTF());
+
+        // CERRAR STREAMS Y SOCKETS
+        flujoEntrada.close();
+        flujoSalida.close();
+        Cliente.close();
+    }// main
+}
